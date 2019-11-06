14945e58da4c3f3218fd63350ad914cbcc4a95ba
LaurinJ <laurin223@seznam.cz>
2019-11-06 21:08:08 +0000

    reggister

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, url_for
 from flask import render_template
 from .database import articles
+from flask import request
+from flask import redirect
+from flask import session
 
 flask_app = Flask(__name__)
+flask_app.secret_key = b'\xb1x@v#\x12\x96@\xe2\xb3~\xe3(\x9f\x96Q\x02-\xf3\x8a\xcd-n\xa6'
 
 @flask_app.route("/")
 def index():
@@ -18,6 +22,8 @@ def view_info():
 
 @flask_app.route("/admin/")
 def view_admin():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
     return render_template('view_admin.html')
 
 @flask_app.route("/blog/<int:art_id>")
@@ -26,6 +32,26 @@ def view_blog(art_id):
     if article:
         return render_template("view_blog.html", article=article)
     return render_template('article_not_found.html', art_id=art_id)
+
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.html")
+
+@flask_app.route("/login/", methods=["POST"])
+def login_user():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "pepa" and password == "12345":
+            session["logged"] = True
+            return redirect(url_for("view_admin"))
+        else:
+            return redirect(url_for("view_login"))
+
+@flask_app.route("/logout/", methods=["POST"])
+def logout_user():
+    session.pop("logged")
+    return redirect(url_for("index"))
 
 # @flask_app.route("/blog/<string:name>/")
 # def blog(name):
