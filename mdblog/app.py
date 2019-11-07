b587c828394674a91af9a1dfe8709379ec62bdcf
LaurinJ <laurin223@seznam.cz>
2019-11-07 21:20:37 +0000

    create database and init/start

--- mdblog/app.py
+++ mdblog/app.py
@@ -4,17 +4,36 @@ from .database import articles
 from flask import request
 from flask import redirect
 from flask import session
+from flask import g
+
+import os
+import sqlite3
 
 flask_app = Flask(__name__)
-flask_app.secret_key = b'\xb1x@v#\x12\x96@\xe2\xb3~\xe3(\x9f\x96Q\x02-\xf3\x8a\xcd-n\xa6'
+
+flask_app.config.from_pyfile("/vagrant/configs/default.py")
+
+if "MDBLOG_CONFIG" in os.environ:
+    flask_app.config.from_envvar("MDBLOG_CONFIG")
 
 @flask_app.route("/")
 def index():
     return render_template("view_home.html")
 
-@flask_app.route("/blogs/")
+@flask_app.route("/blogs/", methods=["GET"])
 def view_blogs():
-    return render_template('view_blogs.html', articles=articles.items())
+    db = get_db()
+    cur = db.execute("select * from articles order by id desc ")
+    articles = cur.fetchall()
+    return render_template('view_blogs.html', articles=articles)
+
+@flask_app.route("/blogs/", methods=["POST"])
+def add_blogs():
+    db = get_db()
+    db.execute("insert into articles (title, content) values (?, ?)",
+               [request.form.get("title"), request.form.get("content")])
+    db.commit()
+    return redirect(url_for("view_blogs"))
 
 @flask_app.route("/info/")
 def view_info():
@@ -28,7 +47,9 @@ def view_admin():
 
 @flask_app.route("/blog/<int:art_id>")
 def view_blog(art_id):
-    article = articles.get(art_id)
+    db = get_db()
+    cur = db.execute("select * from articles where id=(?)",[art_id])
+    article = cur.fetchone()
     if article:
         return render_template("view_blog.html", article=article)
     return render_template('article_not_found.html', art_id=art_id)
@@ -42,7 +63,7 @@ def login_user():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if username == "pepa" and password == "12345":
+        if username == flask_app.config["USERNAME"] and password == flask_app.config["PASSWORD"]:
             session["logged"] = True
             return redirect(url_for("view_admin"))
         else:
@@ -52,6 +73,29 @@ def login_user():
 def logout_user():
     session.pop("logged")
     return redirect(url_for("index"))
+
+# UTILS
+def connect_db():
+    rv = sqlite3.connect(flask_app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, "sqlite.db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@flask_app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+def init_db(app):
+    with app.app_context():
+        db = get_db()
+        with open("mdblog/schema.sql", "r") as fp:
+            db.cursor().executescript(fp.read())
+        db.commit()
 
 # @flask_app.route("/blog/<string:name>/")
 # def blog(name):
