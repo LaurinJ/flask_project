6350ed8c095e7f8c268da18efd9d0a435c483e3b
LaurinJ <laurin223@seznam.cz>
2019-11-26 19:54:26 +0000

    Errors

--- mdblog/mod_blog/controller.py
+++ mdblog/mod_blog/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-
+from flask import request
 
 from mdblog.models import Article
 
@@ -8,8 +8,9 @@ blog = Blueprint("blog", __name__)
 
 @blog.route("/blogs/", methods=["GET"])
 def view_blogs():
-    articles = Article.query.order_by(Article.id.desc())
-    return render_template('mod_blog/view_blogs.html', articles=articles)
+    page = request.args.get("page", 1, type=int)
+    paginate = Article.query.order_by(Article.id.desc()).paginate(page, 5, False)
+    return render_template('mod_blog/view_blogs.html', articles=paginate.items, paginate=paginate)
 
 @blog.route("/blog/<int:art_id>")
 def view_blog(art_id):
